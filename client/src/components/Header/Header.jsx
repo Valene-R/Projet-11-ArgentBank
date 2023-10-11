c966834ed34d2e2f062de5239cd05abbe504efcd
Valene-R <valene.renaud@gmail.com>
2023-10-11 20:35:29 +0200

    feat(User): add username to profile page title

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProfile } from '../../reducers/authSlice'; // Importe le thunk
 import { Root, WelcomeTitle, EditButton, InvisibleHeader } from './Header.styled';
 
-const Header = ({ userName }) => {
-  
+const Header = () => {
+  const dispatch = useDispatch();
+
+  // Utilise le sélecteur pour obtenir les données de l'utilisateur
+  const user = useSelector(state => state.auth.profile);
+
+  useEffect(() => {
+    // Dispatch le thunk pour récupérer les données du profil
+    dispatch(fetchProfile());
+  }, [dispatch]);
+
   return (
     <Root>
-      <WelcomeTitle>Welcome back<br />{userName}!</WelcomeTitle>
+      <WelcomeTitle>Welcome back<br />{user?.firstName} {user?.lastName}!</WelcomeTitle>
       <EditButton>Edit Name</EditButton>
       <InvisibleHeader>Accounts</InvisibleHeader>
     </Root>
